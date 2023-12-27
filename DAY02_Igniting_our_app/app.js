3ca65efc8290792709d24e1352e0872db77df996
Aniket Sinha <sinhaaniket192@gmail.com>
2023-12-27 12:03:36 +0530

    Add files via upload

---
+++ DAY02_Igniting_our_app/app.js
@@ -0,0 +1,28 @@
+/* <div id="parent">
+        <div id="child">
+            <h1>I am h1 tag</h1>
+            <h1> I am h2 tag </h2>
+        </div>
+        <div id="child2">
+            <h1>I am next h1 tag</h1>
+            <h1> I am next h2 tag </h2>
+        </div>
+    </div> */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "ok" }, "I am h1 tag"),
+    React.createElement("h2", { id: "ok" }, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "ok" }, "I am next h1 tag"),
+    React.createElement("h2", { id: "ok" }, "I am next h2 tag"),
+  ]),
+]);
+console.log(heading); //object
+
+const newdiv = ReactDOM.createRoot(document.getElementById("root"));
+
+newdiv.render(heading);
